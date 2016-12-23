8a41549f149dcce1f349a3649ad7c9ece2b7e0d5
Akshay Chiwhane <achiwhane@gmail.com>
2016-12-23 15:52:58 -0500

    complete exercise 3

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -48,13 +48,12 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
+        // DONE (1) Fill in this method to build the proper Github query URL
         Uri builtUri = Uri.parse(GITHUB_BASE_URL)
                 .buildUpon()
                 .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
                 .appendQueryParameter(PARAM_SORT, sortBy)
                 .build();
-
 
         URL url = null;
         try {
